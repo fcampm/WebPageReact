af6e6b1b4c22d50de7e317de2158e6362d8dbd18
fcampm <fabiancamp98@gmail.com>
2020-07-26 20:07:22 -0500

    Página terminada, sujeta a cambios del cliente

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,37 +1,69 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import FaceIcon from '@material-ui/icons/Face';
+import SchoolIcon from '@material-ui/icons/School';
+import ContactSupportIcon from '@material-ui/icons/ContactSupport';
+import InfoIcon from '@material-ui/icons/Info';
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
+import StepperPhotoOne from '../images/carusel_img1.jpg';
+import StepperPhotoTwo from '../images/carusel_img2.jpg';
+import StepperPhotoThree from '../images/carusel_img3.jpg';
+import StepperPhotoFour from '../images/carusel_img4.jpg';
+import StepperPhotoFive from '../images/carusel_img5.png';
+
+function SpaceBuilder(){
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
 
 const tutorialSteps = [
   {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+    label: '"La inclusión solamente se logra trabajando juntos por un mundo mejor."',
+    imgPath: StepperPhotoOne,
+    labelBtn: 'Conócenos',
+    btnEmoji: <FaceIcon fontSize='large'/>,
+    quoteAuthor: 'Meli Pacheco'
   },
   {
-    label: 'Bird',
-    imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+    label: '"Si no generamos conciencia, ¿Quién lo hará?"',
+    imgPath: StepperPhotoTwo,
+    labelBtn: 'Cursos y Capacitaciones',
+    btnEmoji: <SchoolIcon fontSize='large'/>,
+    quoteAuthor: 'Karen Tovar'
   },
   {
-    label: 'Bali, Indonesia',
-    imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
+    label: '"La inclusión comienza en casa, y nosotros somos tu segunda casa."',
+    imgPath: StepperPhotoThree,
+    labelBtn: 'Más Información',
+    btnEmoji: <InfoIcon fontSize='large'/>,
+    quoteAuthor: 'Dany Fernández'
   },
   {
-    label: 'NeONBRAND Digital Marketing, Las Vegas, United States',
-    imgPath:
-      'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
+    label: '"Trabajemos por un mundo donde todas las personas podamos comunicarnos."',
+    imgPath: StepperPhotoFour,
+    labelBtn: 'Sumate',
+    btnEmoji: <PersonAddOutlinedIcon fontSize='large'/>,
+    quoteAuthor: 'Miros Gordillo'
   },
   {
-    label: 'Goč, Serbia',
-    imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+    label: '"Petenece: trabajando por un mundo incluyente y sin barreras de comunicación"',
+    imgPath: StepperPhotoFive,
+    labelBtn: 'Contáctanos',
+    btnEmoji: <ContactSupportIcon fontSize='large'/>,
+    quoteAuthor: 'Ale Cruz'
   },
 ];
 
@@ -42,12 +74,12 @@ const useStyles = makeStyles((theme) => ({
   header: {
     display: 'flex',
     alignItems: 'center',
-    height: 50,
+    height: 100,
     paddingLeft: theme.spacing(4),
     backgroundColor: theme.palette.background.default,
   },
   img: {
-    height: 500,
+    height: 640,
     overflow: 'hidden',
     display: 'block',
     width: '100%',
@@ -71,14 +103,31 @@ export default function HomeComponent(){
 
     return (
         <div className={classes.root}>
-            <Paper square elevation={0} className={classes.header}>
-                <Typography>{tutorialSteps[activeStep].label}</Typography>
-            </Paper>
-            <img
+          <Grid container>
+            <Grid item xs={14} sm={7}>
+              <img
                 className={classes.img}
                 src={tutorialSteps[activeStep].imgPath}
                 alt={tutorialSteps[activeStep].label}
-            />
+              />
+            </Grid>
+            <Grid item xs={2} sm={1}></Grid>
+            <Grid item xs={6} sm={3}>
+              <Box fontStyle='italic'>
+                <Typography variant='h4' align='left'><br />{tutorialSteps[activeStep].label}</Typography>
+              </Box>
+              <Typography variant='h6' align='right'><br />{tutorialSteps[activeStep].quoteAuthor}</Typography>
+              <SpaceBuilder />
+              <SpaceBuilder />
+              <div align='center'>
+                <Button size='large' variant='outlined' color='primary'>
+                  {tutorialSteps[activeStep].labelBtn}
+                  {tutorialSteps[activeStep].btnEmoji}
+                </Button>
+              </div>
+            </Grid>
+            <Grid item xs={2} sm={1}></Grid>
+          </Grid>
             <MobileStepper
                 steps={maxSteps}
                 position="static"
--- src/components/QuienesSomos.js
+++ src/components/QuienesSomos.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import GrupoFondo from '../images/grupo_quienes_somos.jpg';
